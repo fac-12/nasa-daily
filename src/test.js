9366b8537db64857c443965485ed362aa5066849
dragomir <svedava@mail.ru>
2017-11-24 12:07:01 +0000

    removed the test with API call

--- src/test.js
+++ src/test.js
@@ -19,14 +19,14 @@ test('Home route', (t) => {
   })
 })
 
-test('Check response object from 2017-11-23', (t) => {
-  shot.inject(router, { method: 'get', url: '/date=2017-11-23' }, (res) => {
-    var responseObj = JSON.parse(res.payload);
-    t.equal(res.statusCode, 200, 'should respond with a status code of 200');
-    t.deepEquals(responseObj, testObj, 'response object should match test object');
-    t.end();
-  })
-});
+// test('Check response object from 2017-11-23', (t) => {
+//   shot.inject(router, { method: 'get', url: '/date=2017-11-23' }, (res) => {
+//     var responseObj = JSON.parse(res.payload);
+//     t.equal(res.statusCode, 200, 'should respond with a status code of 200');
+//     t.deepEquals(responseObj, testObj, 'response object should match test object');
+//     t.end();
+//   })
+// });
 
 test('Check CSS file is returning', (t) => {
   shot.inject(router, { method: 'get', url: '/public/style.css'}, (res) => {
